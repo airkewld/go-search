0e033c1dad08b16914eb26bc7e77703afeb9b11e
Oscar Rodriguez <oscar@rebelliondefense.com>
2023-04-26 12:36:29 -0400

    update readme and add dagger ci

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # go-search
 Simple app that accepts input and opens your preferred search engine all from the CLI
+
+## Build
+Run `go run ./cicd/ci.go` to build ``linux,darwin, and windows`` binaries (Docker must be running). The binaries will be stored under `./build`.
+```
+build
+├── darwin
+│   └── go-search
+├── linux
+│   └── go-search
+└── windows
+    └── go-search.exe
+```
+
+## Usage
+You can simply run ``./build/<your OS>/go-search`` to get started.
+```
+❯ ./build/darwin/go-search
+AMA: what is today's date
+```
+You may also be prompted to enter your preferred search engine(currently only google and duckduckgo are supported). If you'd like to avoid entering it every time, just set `FAV_SE=duck` as an environment variable.
+
+I mainly use this in my neovim config to start a search from within neovim.
+
+[nvim mapping](https://github.com/airkewld/dotfiles/blob/main/nvim/after/plugin/mappings.lua#L25)
+
+[ansible role](https://github.com/airkewld/dotfiles/blob/main/roles/go-search/tasks/main.yml)
---
+++ cicd/ci.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	if err := build(context.Background()); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("App built successfully!")
+	}
+}
+
+func build(ctx context.Context) error {
+	fmt.Println("Building with Dagger")
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	buildFor := []string{"linux", "darwin", "windows"}
+
+	// get reference to the local project
+	src := client.Host().Directory(".")
+
+	// set images
+	builderImg := "golang:1.20"
+	goSecImg := "securego/gosec"
+
+	// set builder and goSec containers
+	goSec := client.Container().From(goSecImg)
+	builder := client.Container().From(builderImg)
+
+	// mount cloned app path into `builder` image
+	builder = builder.WithDirectory("/src", src).WithWorkdir("/src")
+	// mount cloned app path into `goSec` image
+	goSec = goSec.WithDirectory("/src", src).WithWorkdir("/src")
+
+	// run simple fmt
+    gofmt,err := builder.WithExec([]string{"go", "fmt", "."}).Stdout(ctx)
+    if err!=nil {
+        fmt.Println(err)
+    }else{
+        fmt.Println(gofmt)
+    }
+	// run gosec
+    gosec,err := goSec.WithExec([]string{"gosec", "."}).Stdout(ctx)
+    if err!=nil {
+        fmt.Println(err)
+    }else{
+        fmt.Println(gosec)
+    }
+
+	// define the application build command
+	for _, os := range buildFor {
+		path := fmt.Sprintf("build/%s/", os)
+		builder = builder.WithEnvVariable("GOOS", os)
+		builder = builder.WithExec([]string{"go", "build", "-o", path})
+		// get reference to build output directory in container
+		output := builder.Directory(path)
+		// write contents of container build/ directory to the host
+		_, err = output.Export(ctx, path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
